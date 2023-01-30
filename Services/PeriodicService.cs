f22188bffd493175449170cecf746486b5b97d11
Taufan Augusta <taufan.augusta@protonmail.com>
2023-01-30 23:31:10 +0700

    stash commit

---
+++ Services/PeriodicService.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Hosting;
+
+namespace m0.maui.Services
+{
+    public class PeriodicService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
