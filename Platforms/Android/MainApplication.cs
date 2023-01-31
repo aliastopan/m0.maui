1fb463d122c41dbffd63b57922b2b88ec7cc0b76
Taufan Augusta <taufan.augusta@protonmail.com>
2023-01-31 10:22:47 +0700

    Cleartext traffic

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace m0.maui;
 
-[Application]
+[Application(UsesCleartextTraffic = true)]
 public class MainApplication : MauiApplication
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
